1f6c137b36845f4771e6305f420a70992e272475
Viihtorugo <Victor-Torugo@hotmail.com>
2020-10-09 10:43:32 -0300

    Add image-size and show console

--- first-app/index.js
+++ first-app/index.js
@@ -1,8 +1,18 @@
 const electron = require("electron");
-
-const {app, BrowserWindow} = electron;
+const sizeOf = require('image-size');
+const {app, BrowserWindow, ipcMain} = electron;
 
 app.on("ready", () => {
-   const mainWindow = new BrowserWindow({});
+   const mainWindow = new BrowserWindow({
+       webPreferences: {
+           nodeIntegration: true
+       }
+   });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('getDimImg', (event, path) => {
+    sizeOf(path, function(err, dimensions) {
+        console.log('largura:' + dimensions.width, 'altura:' + dimensions.height);
+    })
 });
